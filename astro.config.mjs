adae53a315ec1a4c6e04483128428fa9e5dad9d7
Philipp Sporrer <philipp.sporrer@celebrate.company>
2024-03-24 13:09:41 +0100

    feat: basic i18n setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,11 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://www.metropoliplan.org',
+    i18n: {
+        defaultLocale: 'de',
+        locales: ['de', 'es'],
+        routing: {
+            prefixDefaultLocale: true,
+        },
+    },
 });
